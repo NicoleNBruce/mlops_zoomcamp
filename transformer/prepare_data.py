9b05f24b49b29f751af6e451541db06fb0959fde
NicoleNBruce <92215797+NicoleNBruce@users.noreply.github.com>
2024-11-12 17:16:29 +0000

    Create prepare_data.py

---
+++ transformer/prepare_data.py
@@ -0,0 +1,30 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import pandas as pd
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    This function converts some string columns into datetime format
+    and generates a new column derived by subtracting the two
+    formatted columns
+
+    Args: data: output from load_data.py
+    """
+
+    data.tpep_dropoff_datetime = pd.to_datetime(data.tpep_dropoff_datetime)
+    data.tpep_pickup_datetime = pd.to_datetime(data.tpep_pickup_datetime)
+
+    data['duration'] = data.tpep_dropoff_datetime - data.tpep_pickup_datetime
+    data.duration = data.duration.dt.total_seconds() / 60
+
+    data = data[(data.duration >= 1) & (data.duration <= 60)]
+
+    #converting the IDs into strings
+    categorical = ['PULocationID', 'DOLocationID']
+    data[categorical] = data[categorical].astype(str)
+    
+    return data
