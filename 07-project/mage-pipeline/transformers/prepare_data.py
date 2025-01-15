1d04626c1c1f9b7372ac1b7291a017715a2849d3
NicoleNBruce <92215797+NicoleNBruce@users.noreply.github.com>
2025-01-15 10:17:55 +0000

    Create prepare_data.py

---
+++ 07-project/mage-pipeline/transformers/prepare_data.py
@@ -0,0 +1,38 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import numpy as np
+
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    This function drops invalid rows from the data and normalises 
+    skewed features.
+
+    Args:
+        data: The output from load_data.py
+
+    Returns:
+        The dataframe and target feature
+    """
+
+    #removing rows that have negative values in columns
+
+    for i in ['SO2', 'PM10']:
+      data = data[data[i] >= 0]
+
+    numerical_columns = data.select_dtypes(exclude= 'object').columns
+
+    #normalising the skewed features with sqrt transformation
+    for i in numerical_columns:
+      data[f"Log_{i}"] = np.sqrt(data[i])
+
+    #encoding the target feature
+    target = data['Air Quality'].map({'Moderate': 0, 'Good': 1, 'Hazardous': 2, 'Poor': 3})
+
+
+    return data, target
